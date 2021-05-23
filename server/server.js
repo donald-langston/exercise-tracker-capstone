e8512e6875f9fb4ba8805f6220ba2acf0179e77d
Donald <donald_langston@yahoo.com>
2021-05-23 02:33:16 -0400

    Changed cors allowed headers

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = ["https://exercise-tracker-capstone.vercel.app"];
 
 app.use(cors({
     origin: allowedOrigins,
-    allowedHeaders: "application/json",
+    allowedHeaders: "Content-Type",
     optionsSuccessStatus: 200 
 }));
 
