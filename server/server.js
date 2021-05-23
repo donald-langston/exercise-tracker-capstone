32a216800846622d605cfdc54a9e22f0994f60f0
Donald <donald_langston@yahoo.com>
2021-05-23 13:47:17 -0400

    Removed trailing slash from frontend url

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ let lastName = "";
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://exercise-tracker-capstone.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "https://exercise-tracker-capstone.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
